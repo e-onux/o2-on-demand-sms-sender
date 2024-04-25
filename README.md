8897393fb9c41dfda0d767a2217fd529f9d78352
Emir Onuk <e.onuk@outlook.com>
2024-04-25 20:35:50 +0200

    Initial commit: Added basic project files including scripts, requirements, and configuration settings

---
+++ README.md
@@ -0,0 +1,52 @@
+# O2 On Demand Hack
+
+This project automates SMS interactions with Huawei LTE modems using the `huawei-lte-api`.
+
+## Installation
+
+1. **Clone the Repository:**
+    ```bash
+    git clone [URL of repository]
+    cd O2-On-Demand-Hack
+    ```
+
+2. **Set Up Python Environment:**
+    It's recommended to use a virtual environment:
+    ```bash
+    python -m venv venv
+    source venv/bin/activate  # On Windows use `venv\Scripts\activate`
+    ```
+
+3. **Install Requirements:**
+    Install the required Python libraries:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Configuration
+
+1. **Environment Variables:**
+    Rename the `.env.example` file to `.env` and edit it to include your credentials:
+    ```bash
+    mv .env.example .env
+    ```
+    Then, open `.env` and update the following entries:
+    ```
+    API_USER=your_username_here
+    API_PASSWORD=your_password_here
+    ```
+
+## Usage
+
+Run the script to interact with the modem:
+```
+python o2_on_demand_hack.py
+```
+
+## Contributing
+
+Feel free to fork this repository and submit pull requests. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This project is licensed under the MIT License see the LICENSE file for details.
---
+++ o2_on_demand_hack.py
@@ -0,0 +1,37 @@
+from huawei_lte_api.Client import Client
+from huawei_lte_api.Connection import Connection
+from huawei_lte_api.AuthorizedConnection import AuthorizedConnection
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Loads the .env file
+
+# Retrieve the username and password from environment variables
+username = os.getenv('API_USER')
+password = os.getenv('API_PASSWORD')
+
+# Enter your modem's IP address and, if necessary, the port number here.
+url = 'http://192.168.8.1/'
+
+# Establishing an authorized connection.
+connection = AuthorizedConnection(url, username=username, password=password)
+
+# Connecting to the API.
+client = Client(connection)
+
+# Retrieve all messages from the SMS inbox.
+sms_list = client.sms.get_sms_list()
+
+# Check for SMS from 80112 that are unread and do not contain a specific text to mark and send.
+exclude_text = "wurde erfolgreich aktiviert"
+send_sms = False
+for sms in sms_list['Messages']['Message']:
+    if sms['Phone'] == '80112' and sms['Smstat'] == '0' and exclude_text not in sms['Content']:
+        # Mark the SMS as read
+        client.sms.set_read(sms['Index'])
+        print(f"From: {sms['Phone']}, Message: {sms['Content']}")
+        send_sms = True
+
+# If there is a relevant SMS, send "WEITER" to 80112.
+if send_sms:
+    client.sms.send_sms(['80112'], 'WEITER')
